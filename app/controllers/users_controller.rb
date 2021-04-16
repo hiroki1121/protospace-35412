27901d541d0d3fe15c220fbd9ded4355c29a9ec6
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-04-16 20:21:40 +0900

    userページ　表示修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @prototype = Prototype.all
+    @prototype = Prototype.where(user_id: @user)
   end
 end
