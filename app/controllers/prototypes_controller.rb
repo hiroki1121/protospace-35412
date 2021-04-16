00a56d40f90a6c86da8887762a470d2428a4fa74
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-04-16 21:07:37 +0900

    画面遷移修正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,13 +54,13 @@ class PrototypesController < ApplicationController
   def move_to_index_edit
     @prototype = Prototype.find(params[:id])
     unless user_signed_in? && current_user.id == @prototype.user.id
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
   end
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
   end
 
