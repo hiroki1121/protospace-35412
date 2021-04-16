f5c2f61bc604a5ba358e00c5d5024857c1ef1a73
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-04-16 20:58:47 +0900

    画面遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :new, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index_edit, only: [:edit]
+  before_action :move_to_index, only: [:new, :destroy]
 
   def index
     @prototype = Prototype.all
@@ -50,10 +51,17 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def move_to_index_edit
     @prototype = Prototype.find(params[:id])
     unless user_signed_in? && current_user.id == @prototype.user.id
       redirect_to action: :index
     end
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
